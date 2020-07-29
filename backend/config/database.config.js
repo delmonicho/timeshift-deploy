d26d68987a406a67641a98beb08d7d6300b8941d
Nicho Delmo <ndelmoral13@gmail.com>
2020-07-29 11:19:49 -1000

    Update database.config.js

--- backend/config/database.config.js
+++ backend/config/database.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // url/connection string
-  url: 'mongodb+srv://tShift:timeshift@timeshift-bkhcn.mongodb.net/entries?retryWrites=true&w=majority',
+  url: 'mongodb+srv://$$$$$.mongodb.net/entries?retryWrites=true&w=majority',
   secret: "dontThinkAboutIt"
 }
